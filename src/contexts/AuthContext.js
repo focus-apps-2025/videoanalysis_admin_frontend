61e3e323462c52c9e76369e5649b4a7a19fc2f9d
focus-apps-2025 <focusenggapps@gmail.com>
2025-11-03 21:46:14 +0530

    Update AuthContext.js

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -4,7 +4,7 @@ import { setAuthToken } from '../services/api';
 
 export const AuthContext = createContext(null);
 
-const API_BASE = 'http://72.60.96.50:5000';
+const API_BASE = 'https://72.60.96.50';
 
 export default function AuthProvider({ children }) {
   const [token, setToken] = useState(null);
